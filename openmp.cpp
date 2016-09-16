0384735eb3b858166d09116e00c237347b9ccb36
Ilnur Yakupov <yakupnk@gmail.com>
2016-09-16 22:20:22 +0300

    openmp

--- openmp.cpp
+++ openmp.cpp
@@ -5,11 +5,14 @@
 #include <cstdlib>
 #include <ctime>
 #include <algorithm>
+#include <omp.h>
+#include <time.h>
 
 //Matrix multiplication    
 int main(int argc, char *argv[])
 {
     int n = 1024;
+    time_t startTime,endTime;
 
     std::vector<double> a(n*n);
     std::vector<double> b(n*n);
@@ -20,10 +23,23 @@ int main(int argc, char *argv[])
     std::generate(a.begin(), a.end(), std::rand);
     std::generate(b.begin(), b.end(), std::rand);
 
+    time (&startTime);
     for(int i = 0; i < n; ++i)
         for(int j = 0; j < n; ++j)
             for(int k = 0; k < n; ++k)
                 c[i*n + j] += a[i*n + k] * b[k*n + j];
-	
+   
+    time (&endTime); 
+    std::cout << difftime (endTime,startTime) << std::endl;
+
+    time (&startTime);
+    #pragma omp parallel for
+    for(int i = 0; i < n; ++i)
+        for(int j = 0; j < n; ++j)
+            for(int k = 0; k < n; ++k)
+                c[i*n + j] += a[i*n + k] * b[k*n + j];
+   
+    time (&endTime); 
+    std::cout << difftime (endTime,startTime) << std::endl;
     return 0;
 }
